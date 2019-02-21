2c1e19b2df18adfdfab25220ed04adaafebefebb
Rocio <act.rocio.avila@gmail.com>
2019-02-21 12:46:45 -0600

    delete 1

--- dplyr_db/R/context.R
+++
@@ -1,5 +0,0 @@
-context_env <- new_environment()
-
-from_context <- function(what){
-  context_env[[what]] %||% abort(glue("{expr} should only be called in a data context", expr = deparse(sys.call(-1))))
-}
